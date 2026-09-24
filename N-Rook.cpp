3fb949a32106ecfbcfcf1789d8fab088e4c493a8
Mahi Singh <mahihnew@gmail.com>
2026-09-24 23:56:35 +0530

    Add N-Rook solution

---
+++ N-Rook.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isSafe(vector<vector<int>>& board, int row, int col, int n) {
+    for (int i = 0; i < row; i++) {
+        if (board[i][col] == 1)
+            return false;
+    }
+
+    return true;
+}
+
+bool solveNRooks(vector<vector<int>>& board, int row, int n) {
+    if (row == n)
+        return true;
+
+    for (int col = 0; col < n; col++) {
+        if (isSafe(board, row, col, n)) {
+            board[row][col] = 1;
+
+            if (solveNRooks(board, row + 1, n))
+                return true;
+
+            board[row][col] = 0;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> board(n, vector<int>(n, 0));
+
+    if (solveNRooks(board, 0, n)) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << (board[i][j] ? "R " : ". ");
+            }
+            cout << endl;
+        }
+    } else {
+        cout << "No solution exists." << endl;
+    }
+
+    return 0;
+}
